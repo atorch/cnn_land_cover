244389c4cb2ee3600883c41a29c81052b0cec86d
Adrian Torchiana <adrian.torchiana@gmail.com>
2019-03-29 19:49:50 -0700

    Add a county to road download

--- download_road_shapefiles.sh
+++ download_road_shapefiles.sh
@@ -4,7 +4,7 @@ mkdir -p roads
 
 cd roads
 
-for county in 17011 55023 55103
+for county in 17011 17103 55023 55103
 do
     file=tl_2017_${county}_roads.zip
     wget ftp://ftp2.census.gov/geo/tiger/TIGER2017/ROADS/$file --no-clobber
